cf4e1b8addc27be7f9808a5c2739105b086d2c61
fjhuete <fjhuete.m@gmail.com>
2024-04-18 21:33:42 +0200

    Añadidas las funciones de ayuda y versión y la función de control de argumentos.

--- IPActivas_IPDNS.sh
+++ IPActivas_IPDNS.sh
@@ -37,19 +37,33 @@ fin_formato="\e[0m"
 #Zona de declaración de funciones
 
 mostrar_ayuda() {
-echo "Uso: plantilla.sh [ARGUMENTOS]
-Descripción: 
+echo "Uso: $0 [IP | Dirección de red | -i FICHERO][-o FICHERO]
+Descripción: Recibe un rango de direcciones IP y lista las que están activas y las que están en el DNS.
 Parámetros aceptados:
-	- 
-	- 
-	- "
+	-i <FICHERO>	Lee las direcciones IP de un fichero. Si no se indica, toma como dirección IP el primer argumento.
+	-o <FICHERO>	Escribe la salida a un fichero. Si no se indica, muestra el resultado por la salida estándar.
+	-h 		Muestra esta ayuda.
+	-v 		Muestra la versión."
 }
 
 mostrar_version() {
-echo "plantilla.sh Versión: 1.0"
+echo "$0 Versión: 1.0"
 exit 0
 }
 
 #Zona del script
 
+#Control de argumentos
+while getopts "iohv" opcion; do
+	case $opcion in
+		i) leer_fichero ;;
+		o) escribir_fichero ;;
+		h) mostrar_ayuda; exit 0;;
+		v) mostrar_version ;;
+		?) mostrar_ayuda; exit 1
+	esac
+done
 
+if [ $OPTIND -eq 1 ]; then #Esto hay que arreglarlo
+	echo -e $rojo"$0 requiere un argumento."$fin_formato >&2; mostrar_ayuda; exit 1
+fi
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# script_DNS
-Script en pareja como práctica para la asignatura Implantación de Sistemas Operativos
+# Proyecto de script
+Este proyecto consiste en la programación de un script en pareja como práctica para la asignatura Implantación de Sistemas Operativos.
+
+## Objetivo
+Este script solicita un rango de direcciones IP y se responde: cuáles están activas y cuáles están en el DNS.
